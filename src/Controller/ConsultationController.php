5c5deab2fefb1d436af4b1b8988267544c563d80
ranya <ranya.ouesleti@esprit.tn>
2026-04-09 15:47:02 +0100

    crud working

--- src/Controller/ConsultationController.php
+++ src/Controller/ConsultationController.php
@@ -44,18 +44,13 @@ final class ConsultationController extends AbstractController
         return $this->redirectToRoute('app_post_show', ['id' => $id], Response::HTTP_MOVED_PERMANENTLY);
     }
 
-    #[Route('/consultation', name: 'app_consultation', methods: ['GET', 'POST'])]
-    public function index(
+    #[Route('/consultation/new', name: 'app_post_new', methods: ['GET', 'POST'])]
+    public function new(
         Request $request,
-        PostRepository $posts,
         EntityManagerInterface $em,
     ): Response {
         $user = $this->requireForumRole();
-        $q = $request->query->getString('q');
-        $filterCategorie = $request->query->getString('categorie');
-        $page = max(1, $request->query->getInt('page', 1));
-
-        $categorySuggestions = $posts->findDistinctCategories();
+        $categorySuggestions = PostConsultationFormType::getCategoryChoices();
 
         $post = new Post();
         $post->setAuteur($user);
@@ -73,18 +68,27 @@ final class ConsultationController extends AbstractController
             $post->setDate(new \DateTime());
             $em->persist($post);
             $em->flush();
-            $this->addFlash('success', 'Votre publication a été enregistrée.');
+            $this->addFlash('success', 'Votre publication a été enregistrée sur le forum.');
 
-            $redirectParams = [];
-            if ($q !== '') {
-                $redirectParams['q'] = $q;
-            }
-            if ($filterCategorie !== '') {
-                $redirectParams['categorie'] = $filterCategorie;
-            }
-
-            return $this->redirectToRoute('app_consultation', $redirectParams);
+            return $this->redirectToRoute('app_consultation');
         }
+
+        return $this->render('consultation/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/consultation', name: 'app_consultation', methods: ['GET'])]
+    public function index(
+        Request $request,
+        PostRepository $posts,
+    ): Response {
+        $this->requireForumRole();
+        $q = $request->query->getString('q');
+        $filterCategorie = $request->query->getString('categorie');
+        $page = max(1, $request->query->getInt('page', 1));
+
+        $categorySuggestions = PostConsultationFormType::getCategoryChoices();
 
         $result = $posts->searchConsultationsPaginated(
             $q !== '' ? $q : null,
@@ -96,11 +100,9 @@ final class ConsultationController extends AbstractController
 
         return $this->render('consultation/index.html.twig', [
             'posts' => $result['items'],
-            'form' => $form,
             'q' => $q,
             'filter_categorie' => $filterCategorie,
             'category_suggestions' => $categorySuggestions,
-            'show_post_modal' => $form->isSubmitted() && !$form->isValid(),
             'page' => $page,
             'total_pages' => $totalPages,
         ]);
@@ -155,6 +157,116 @@ final class ConsultationController extends AbstractController
             'commentFormOpen' => $form->isSubmitted() && !$form->isValid(),
             'replyTarget' => $comment->getParent(),
         ]);
+    }
+
+    #[Route('/consultation/{id}/like', name: 'app_post_like', methods: ['GET'])]
+    public function likePost(int $id, Request $request, PostRepository $posts, EntityManagerInterface $em): RedirectResponse
+    {
+        $this->requireForumRole();
+        $post = $posts->find($id);
+        if ($post) {
+            $session = $request->getSession();
+            $likedPosts = $session->get('liked_posts', []);
+
+            if (!in_array($id, $likedPosts)) {
+                $post->setNbLikes($post->getNbLikes() + 1);
+                $likedPosts[] = $id;
+                $session->set('liked_posts', $likedPosts);
+                $em->flush();
+                $this->addFlash('success', 'Vous aimez cette publication !');
+            } else {
+                $this->addFlash('warning', 'Vous avez déjà aimé cette publication.');
+            }
+        }
+
+        $referer = $request->headers->get('referer');
+        if ($referer !== null) {
+            return $this->redirect($referer);
+        }
+
+        return $this->redirectToRoute('app_post_show', ['id' => $id]);
+    }
+
+    #[Route('/consultation/{id}/edit', name: 'app_post_edit', methods: ['GET', 'POST'])]
+    public function edit(
+        int $id,
+        Request $request,
+        PostRepository $posts,
+        EntityManagerInterface $em
+    ): Response {
+        $user = $this->requireForumRole();
+        $post = $posts->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('Publication introuvable.');
+        }
+
+        if ($post->getAuteur()?->getId() !== $user->getId()) {
+            throw new AccessDeniedException('Vous n\'êtes pas l\'auteur de cette publication.');
+        }
+
+        $categorySuggestions = PostConsultationFormType::getCategoryChoices();
+        $form = $this->createForm(PostConsultationFormType::class, $post, [
+            'category_choices' => $categorySuggestions,
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Votre publication a été modifiée.');
+
+            return $this->redirectToRoute('app_post_show', ['id' => $post->getId()]);
+        }
+
+        return $this->render('consultation/edit.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/consultation/{id}/delete', name: 'app_post_delete', methods: ['POST'])]
+    public function delete(
+        int $id,
+        Request $request,
+        PostRepository $posts,
+        EntityManagerInterface $em
+    ): RedirectResponse {
+        $user = $this->requireForumRole();
+        $post = $posts->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('Publication introuvable.');
+        }
+
+        if ($post->getAuteur()?->getId() !== $user->getId()) {
+            throw new AccessDeniedException('Vous n\'êtes pas l\'auteur de cette publication.');
+        }
+
+        if ($this->isCsrfTokenValid('delete' . $post->getId(), $request->request->get('_token'))) {
+            $em->remove($post);
+            $em->flush();
+            $this->addFlash('success', 'Votre publication a été supprimée.');
+        }
+
+        return $this->redirectToRoute('app_consultation');
+    }
+
+    #[Route('/consultation/commentaire/{id}/like', name: 'app_comment_like', methods: ['GET'])]
+    public function likeComment(int $id, Request $request, CommentaireRepository $commentaires, EntityManagerInterface $em): RedirectResponse
+    {
+        $this->requireForumRole();
+        $comment = $commentaires->find($id);
+        if ($comment) {
+            $comment->setNbLikes($comment->getNbLikes() + 1);
+            $em->flush();
+        }
+
+        $referer = $request->headers->get('referer');
+        if ($referer !== null) {
+            return $this->redirect($referer);
+        }
+
+        return $this->redirectToRoute('app_post_show', ['id' => $comment?->getPost()?->getId() ?? 0]);
     }
 
     #[Route('/mentions-legales', name: 'app_mentions_legales')]
